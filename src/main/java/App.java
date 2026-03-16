290e8a6fa76e611001f580104f02d4d2d9c54854
Ajith Kumar <ajith.kumar@wisseninfotech.com>
2026-03-17 00:17:29 +0530

    main file modified

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,10 +1,24 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import com.sun.net.httpserver.HttpServer;
+
 public class App {
 
-    public static void main(String[] args) {
-        System.out.println("CI/CD Java application running successfully!");
-    }
+    public static void main(String[] args) throws IOException {
 
-    public static int add(int a, int b) {
-        return a + b;
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
+        server.createContext("/", exchange -> {
+            String response = "CICD pipeline working!";
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        });
+
+        server.start();
+
+        System.out.println("Server started on port 8080");
     }
 }
